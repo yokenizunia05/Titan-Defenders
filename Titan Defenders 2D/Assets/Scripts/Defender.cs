9a7c2d2190922ebc6a530c4b94ce6cc4b4be72dc
Yoken <shunhenson1@gmail.com>
2020-11-15 13:18:36 -0500

    Health Bar and damage update

--- Titan Defenders 2D/Assets/Scripts/Defender.cs
+++ Titan Defenders 2D/Assets/Scripts/Defender.cs
@@ -4,20 +4,45 @@ using UnityEngine;
 
 public class Defender : MonoBehaviour
 {
-    
+    public GameObject defender;
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
     private Vector2 target;
     public float speed;
-    // Start is called before the first frame update
+    
+
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        target = new Vector2(10, 0);
+        target = new Vector2(10, transform.position.y);
         transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        if(currentHealth <= 0)
+        {
+            Destroy(defender);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            TakeDamage(5);
+        }
     }
 }
--- Titan Defenders 2D/Assets/Scripts/Enemy.cs
+++ Titan Defenders 2D/Assets/Scripts/Enemy.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public Transform defenderPos;
+    public HealthBar healthBar;
+    public int maxHealth = 100;
+    public int currentHealth;
     public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        defenderPos = GameObject.FindGameObjectWithTag("Defender").GetComponent<Transform>();
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, defenderPos.position, speed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(-10,0), speed * Time.deltaTime);
+
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            TakeDamage(10);
+        }
     }
 }
