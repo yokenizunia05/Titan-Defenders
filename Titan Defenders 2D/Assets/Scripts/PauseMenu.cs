51dfdfa4f8d642d0ff2d4b68967624ac175da94f
Yoken <shunhenson1@gmail.com>
2020-11-20 16:21:00 -0500

    Main and Pause menu update

---
+++ Titan Defenders 2D/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                resume();
+            }
+            else
+            {
+                pause();
+            }
+        }
+    }
+     public void resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+    void pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void loadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void quitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quitting game...");
+    }
+}
